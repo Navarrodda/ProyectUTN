62bdf3d2843e4213dc6fcdd718561c4cde459e68
Navarrodda <daviddd.dn@gmail.com>
2020-05-29 20:41:14 -0300

    Creation of models whith their attributes

--- src/main/java/utn/project/domain/City.java
+++ src/main/java/utn/project/domain/City.java
@@ -1,4 +1,26 @@
 package utn.project.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "cities")
 public class City {
+
+    private Integer id;
+
+    private String name;
+
+    private  String pre_fil_ex;
+
+    private  State id_state;
+
 }
