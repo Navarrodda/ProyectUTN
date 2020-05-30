309eacdf609b99f0d6a2a7783ee5477e7213aa08
Navarrodda <daviddd.dn@gmail.com>
2020-05-30 20:38:04 -0300

    Add Id and Integers models

--- src/main/java/utn/project/domain/User.java
+++ src/main/java/utn/project/domain/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @NoArgsConstructor
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Entity(name = "users")
 public class User {
 
+    @Id
     private Integer id;
     private String name;
     private String surname;
