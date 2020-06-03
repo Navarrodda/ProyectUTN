02204888b7e8d9c33d01974c5b524647ee0b340d
Navarrodda <daviddd.dn@gmail.com>
2020-06-03 12:48:05 -0300

    Modification dependensis in domains

--- src/main/java/utn/project/domain/User.java
+++ src/main/java/utn/project/domain/User.java
@@ -1,7 +1,6 @@
 package utn.project.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
