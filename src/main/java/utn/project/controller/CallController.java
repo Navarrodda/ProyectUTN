4c96516e4dba203b899e9ebcff5fd326767de1b1
Navarrodda <daviddd.dn@gmail.com>
2020-06-15 11:02:27 -0300

    Modify and creeate controller get and pos call

--- src/main/java/utn/project/controller/CallController.java
+++ src/main/java/utn/project/controller/CallController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import utn.project.domain.Call;
-import utn.project.domain.City;
-import utn.project.projections.CityMoreState;
 import utn.project.service.CallService;
-import utn.project.service.CityService;
 
 import java.util.List;
 
@@ -24,27 +21,12 @@ public class CallController {
         this.callService = callService;
     }
 
-    @GetMapping("/city")
+    @GetMapping("/")
     public List<Call> getCall(){
         return callService.getCall();
     }
 
-    @PostMapping("/city/start")
-    public void addCall(@RequestBody Call call){
+    @PostMapping("/")
+    public void addCall(@RequestBody Call call){ callService.add(call);}
 
-
-
-        callService.add(call);
-    }
-
-    
-   /* @PostMapping("/city/end")
-    public void addCall(@RequestBody Call call){ callService.add(call);
-    }*/
-
-
-   /* public int minuteCounter(){
-
-        return 1;
-    }*/
 }
